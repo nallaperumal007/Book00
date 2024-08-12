ba85e6b1a2fa9bc98f4f53ae3e7b2e78a04b2f95
nallaperumal007 <nallaperumal342@gmail.com>
2024-08-12 09:58:10 +0530

    4

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-docs',
+  "stories": [
+    "../src/**/*.stories.mdx"
   ],
-};
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    '@storybook/addon-postcss',
+    'storybook-css-modules-preset',
+  ]
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
 export * from './Tablr';
-export * from './My';
